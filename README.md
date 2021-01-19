d0709919b81680c1156969856966d752dc5e2aa0
purushothaman-source <purushoth99jun@gmail.com>
2021-01-19 20:07:47 +0530

    ADDED : Welcome message

---
+++ README.md
@@ -0,0 +1,1 @@
+This Project is used to create Address book
---
+++ src/com/uccases/AddressBook.java
@@ -0,0 +1,6 @@
+package com.uccases;
+public class AddressBook {
+    public static void main(String[] args) {
+        System.out.println("Welcome to AddressBook");
+    }
+}
