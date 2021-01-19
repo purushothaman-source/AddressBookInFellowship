5acb2a35e69f7f23960dd88942b4085377b5d56c
purushothaman-source <purushoth99jun@gmail.com>
2021-01-19 20:23:05 +0530

    ADDED : code to add Contacts

--- src/com/uccases/AddressBook.java
+++ src/com/uccases/AddressBook.java
@@ -1,6 +1,26 @@
 package com.uccases;
+
+import java.util.ArrayList;
+
+class Contacts {
+    String firstName, lastName, address, city, state;
+    long zip, phoneNumber;
+
+    public Contacts(String firstName, String lastName, String address, String city, String state, long zip,
+                    long phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+    }
+}
+
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook");
+        ArrayList<Contacts> book = new ArrayList<>();
     }
 }
