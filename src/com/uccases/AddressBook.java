85e051bdb557dc9fe9ef0fd180768cc1f1c43976
purushothaman-source <purushoth99jun@gmail.com>
2021-01-19 20:42:01 +0530

    ADDED : code to add Contacts in book

--- src/com/uccases/AddressBook.java
+++ src/com/uccases/AddressBook.java
@@ -1,6 +1,7 @@
 package com.uccases;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Contacts {
     String firstName, lastName, address, city, state;
@@ -16,11 +17,38 @@ class Contacts {
         this.zip = zip;
         this.phoneNumber = phoneNumber;
     }
+
+    @Override
+    public String toString() {
+        return "Contacts{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip=" + zip +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
 }
 
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook");
         ArrayList<Contacts> book = new ArrayList<>();
+        add(book);
+        System.out.println(book);
     }
+    public static void add(ArrayList<Contacts> book){
+        Scanner input = new Scanner(System.in);
+        String firstName = input.next();
+        String lastName = input.next();
+        String address = input.next();
+        String city = input.next();
+        String state = input.next();
+        long zip = input.nextLong();
+        long phoneNumber = input.nextLong();
+        book.add(new Contacts(firstName,lastName,address,city,state,zip,phoneNumber));
+    }
+
 }
